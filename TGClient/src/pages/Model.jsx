ff796e7b09829621706937527962434308894d7d
Anish_K3 <anishkhot333@gmail.com>
2023-05-01 01:34:44 +0530

    added model in mynft

--- TGClient/src/pages/Model.jsx
+++ TGClient/src/pages/Model.jsx
@@ -1,71 +1,57 @@
-import React, { useState } from "react";
-import { fashare } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 
-function MyModal(props) {
-  const [showModal, setShowModal] = useState(false);
-
-  function handleButtonClick() {
-    setShowModal(true);
-  }
-
-  function handleCloseModal() {
-    setShowModal(false);
-  }
-  {
-    <i class="fa fa-share-alt" aria-hidden="true"></i>;
-  }
-  const [email, setEmail] = useState("");
+function MyModal() {
+  const [show, setShow] = useState(false);
+  const [email, setEmail] = useState('');
   const [number, setNumber] = useState('');
 
-  function handleEmailChange(event) {
-    setEmail(event.target.value);
-  }
-
-  function handleTimeChange(event) {
-    setNumber(event.target.value);
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log(`Email: ${email} Days: ${number} cid: ${props.cid}`);
-    // Here you can handle the form submission, such as sending the data to a server
-  }
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleNumberChange = (e) => {
+    setNumber(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShow(false)
+    console.log(`Email: ${email}, Number: ${number}`);
+  };
   return (
-    <div>
-      <button style={{ marginTop: "100px",display: "inline-flex",border: "none",outline: "none",padding: "5px 22px",background: "transparent",fontsize: "0.9rem",color:" #fff",margin: "0 20px",width: "170px",fontsize: "large",borderRadius: "5px",transition: "0.3s",background: "#2684ff",textDecoration: "none",justifyContent: "space-evenly"}} onClick={handleButtonClick} class="share button">
-        <i class="fa fa-share-alt" aria-hidden="true"></i> Share
-      </button>
-      {showModal && (
-        <div className="modal-container">
-          <div className="modal-content">
-            <span className="close" onClick={handleCloseModal}>
-              &times;
-            </span>
-            <p>
-              <form onSubmit={handleSubmit}>
-                <label>
-                  Email:
-                  <input
-                    type="email"
-                    value={email}
-                    onChange={handleEmailChange}
-                  />
-                </label>
-                <br />
-                <label>
-                  To be shared for:
-                  <input type="number" value={number} onChange={handleTimeChange} />
-                </label>
-                <br />
-                <button type="submit">Submit</button>
-              </form>
-            </p>
-          </div>
-        </div>
-      )}
-    </div>
+    <>
+      <Button variant="primary" class="sharebtn" onClick={handleShow} style={{position:"relative",left:"85%",top:"100px",padding:"10px 40px"}}>
+      <i class="fa fa-share-alt" aria-hidden="true"></i> Share
+      </Button>
+
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Give NFT Access To</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleSubmit}>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address:</label>
+              <input type="email" value={email} onChange={handleEmailChange} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+              {/* <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> */}
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Number of days:</label>
+              <input type="tel" value={number} onChange={handleNumberChange} class="form-control" id="exampleInputPassword1" placeholder="Number of days" />
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" type='submit' onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
-
 export default MyModal;
